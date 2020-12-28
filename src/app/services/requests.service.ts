c481fdeb5e11f0c5f619c4b0fe1de1f18616c653
Daniel Medina <daniel@medina.com>
2020-12-28 10:12:41 -0600

    Prototyped the getProjects() method on requests.service.ts

--- src/app/services/requests.service.ts
+++ src/app/services/requests.service.ts
@@ -2,12 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
+import { Global } from './global';
 
 @Injectable()
 export class RequestsService {
-    constructor(private http: HttpClient) { }
+
+    public url: string;
+
+    constructor(private http: HttpClient) {
+        this.url = Global.url;
+    }
 
     getProjects() {
-        
+        return this.http.get(this.url).subscribe(
+            response => {
+                console.log(response);
+            },
+            error => {
+                console.log(error);
+            }
+        );
     }
 }
